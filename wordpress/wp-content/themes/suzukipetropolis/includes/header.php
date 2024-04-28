778e27dff9edfc0d3d84e39eb02ff24124ead9d1
Elton Leão <elton.leao@peopleinteractive.com.br>
2024-04-28 20:15:27 -0300

    Update AOS animation configuration in courses.php

--- wordpress/wp-content/themes/suzukipetropolis/includes/header.php
+++ wordpress/wp-content/themes/suzukipetropolis/includes/header.php
@@ -7,11 +7,15 @@
 
 						<!-- Crest & Logo -->
 						<div class="logo">
-							<div class="btCrest"><a href="https://tabula.bold-themes.com/wavy/"><img src="<?= get_template_directory_uri() ?>/assets/crest_music.png" class="btCrestImg" alt="Wavy"></a></div> <span>
-								<a href="https://tabula.bold-themes.com/wavy/"><img class="btMainLogo" data-hw="2.4444444444444" src="<?= get_template_directory_uri() ?>/assets/logo_text_01.png" alt="Wavy"></a>
-							</span>
+							<div class="btCrest">
+								<a href="/">
+									<img 
+									src="<?= get_template_directory_uri() ?>/assets/image/logo-suzukipetropolis.svg" 
+									class="btCrestImg mainLogo" alt="Wavy">
+								</a>
+							</div> 
+							
 						</div>
-
 						<!-- Only Crest -->
 
 						<div class="topBarInLogoArea">
@@ -37,14 +41,16 @@
 						<div class="menuPort">
 							<div class="topBarInMenu">
 								<div class="topBarInMenuCell">
-									<a href="https://twitter.com/bold_themes" title="" target="_blank" class="btIconWidget ">
-										<div class="btIconWidgetIcon"><span data-ico-fontawesome="" class="bt_bb_icon_holder"></span></div>
-									</a><a href="https://www.facebook.com/boldthemes/" title="" target="_blank" class="btIconWidget ">
-										<div class="btIconWidgetIcon"><span data-ico-fontawesome="" class="bt_bb_icon_holder"></span></div>
-									</a><a href="https://www.youtube.com/channel/UCPL5LgX32KsbQ1i9nV-xiKA" title="" target="_blank" class="btIconWidget ">
-										<div class="btIconWidgetIcon"><span data-ico-fontawesome="" class="bt_bb_icon_holder"></span></div>
-									</a><a href="https://tabula.bold-themes.com/wavy/home/home-music-courses/#" title="" target="_self" class="btIconWidget ">
+									<a target="_blank" href="https://www.instagram.com/suzukipetropolis/" title="" target="_self" class="btIconWidget ">
 										<div class="btIconWidgetIcon"><span data-ico-fontawesome="" class="bt_bb_icon_holder"></span></div>
+									</a>
+									<a target="_blank" href="https://www.facebook.com/profile.php?id=100063468942821" title="" target="_blank" class="btIconWidget ">
+										<div class="btIconWidgetIcon">
+											<span data-ico-fontawesome="" class="bt_bb_icon_holder"></span>
+										</div>
+									</a>
+									<a target="_blank" href="https://www.youtube.com/@centrodeartessuzuki-petrop7723" title="" target="_blank" class="btIconWidget ">
+										<div class="btIconWidgetIcon"><span data-ico-fontawesome="" class="bt_bb_icon_holder"></span></div>
 									</a>
 								</div><!-- /topBarInMenu -->
 							</div><!-- /topBarInMenuCell -->
@@ -52,220 +58,17 @@
 								<ul id="menu-primary-menu" class="menu">
 									<li id="menu-item-1549" class="btMenuWideDropdown menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-page-ancestor current-menu-ancestor current-page-parent current_page_ancestor menu-item-has-children menu-item-1549 btMenuWideDropdownCols-3">
 										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/">Home</a>
-										<ul class="sub-menu">
-											<li id="menu-item-3083" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-3083">
-												<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/home/home-music-courses/#">Wavy</a>
-												<ul class="sub-menu">
-													<li id="menu-item-2437" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-page-ancestor current-page-parent menu-item-2437">
-														<a href="https://tabula.bold-themes.com/wavy/">Language</a>
-													</li>
-													<li id="menu-item-2354" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-2351 current_page_item menu-item-2354">
-														<a href="https://tabula.bold-themes.com/wavy/home/home-music-courses/" aria-current="page">Music Courses</a>
-													</li>
-													<li id="menu-item-2295" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2295">
-														<a href="https://tabula.bold-themes.com/wavy/home/home-music-school/">Music
-															School</a>
-													</li>
-													<li id="menu-item-2498" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2498">
-														<a href="https://tabula.bold-themes.com/wavy/home/home-ballet/">Ballet</a>
-													</li>
-												</ul>
-											</li>
-											<li id="menu-item-3078" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-3078">
-												<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/home/home-music-courses/#">Sunny</a>
-												<ul class="sub-menu">
-													<li id="menu-item-3079" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3079">
-														<a href="https://tabula.bold-themes.com/sunny/">Draw &amp;
-															Paint</a>
-													</li>
-													<li id="menu-item-3080" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3080">
-														<a href="https://tabula.bold-themes.com/sunny/home/home-illustration/">Illustration</a>
-													</li>
-													<li id="menu-item-3081" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3081">
-														<a href="https://tabula.bold-themes.com/sunny/home/home-language/">Language</a>
-													</li>
-													<li id="menu-item-3082" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3082">
-														<a href="https://tabula.bold-themes.com/sunny/home/home-acting/">Acting</a>
-													</li>
-												</ul>
-											</li>
-											<li id="menu-item-3094" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-3094">
-												<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/home/home-music-courses/#">Shady</a>
-												<ul class="sub-menu">
-													<li id="menu-item-3095" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3095">
-														<a href="https://tabula.bold-themes.com/shady/">Music</a>
-													</li>
-													<li id="menu-item-3096" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3096">
-														<a href="https://tabula.bold-themes.com/shady/home/home-contemporary-ballet/">Contemporary
-															Ballet</a>
-													</li>
-													<li id="menu-item-3097" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3097">
-														<a href="https://tabula.bold-themes.com/shady/home/home-jazz-ballet/">Jazz
-															Ballet</a>
-													</li>
-													<li id="menu-item-3098" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3098">
-														<a href="https://tabula.bold-themes.com/shady/home/home-acting/">Acting</a>
-													</li>
-												</ul>
-											</li>
-										</ul>
 									</li>
 									<li id="menu-item-43" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-43">
-										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/about-us/">About</a>
-										<ul class="sub-menu">
-											<li id="menu-item-2593" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2593">
-												<a href="https://tabula.bold-themes.com/wavy/about-us/">About Us</a>
-											</li>
-											<li id="menu-item-2603" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2603">
-												<a href="https://tabula.bold-themes.com/wavy/about-us/about-me/">About
-													Me</a>
-											</li>
-											<li id="menu-item-2187" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2187">
-												<a href="https://tabula.bold-themes.com/wavy/about-us/our-team/">Our
-													Team</a>
-											</li>
-											<li id="menu-item-2606" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2606">
-												<a href="https://tabula.bold-themes.com/wavy/about-us/history/">History</a>
-											</li>
-											<li id="menu-item-2609" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2609">
-												<a href="https://tabula.bold-themes.com/wavy/about-us/testimonials/">Testimonials</a>
-											</li>
-											<li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42">
-												<a href="https://tabula.bold-themes.com/wavy/about-us/contact/">Contact</a>
-											</li>
-											<li id="menu-item-2604" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2604">
-												<a href="https://tabula.bold-themes.com/wavy/about-us/coming-soon/">Coming
-													Soon</a>
-											</li>
-											<li id="menu-item-2724" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2724">
-												<a href="https://tabula.bold-themes.com/wavy/404">404</a>
-											</li>
-										</ul>
+										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/about-us/">Quem somos</a>
 									</li>
 									<li id="menu-item-2168" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2168">
-										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/courses/">Courses</a>
-										<ul class="sub-menu">
-											<li id="menu-item-2610" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2610">
-												<a href="https://tabula.bold-themes.com/wavy/courses/">Courses</a>
-											</li>
-											<li id="menu-item-2241" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2241">
-												<a href="https://tabula.bold-themes.com/wavy/courses/single-course/">Single
-													Course</a>
-											</li>
-											<li id="menu-item-2207" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2207">
-												<a href="https://tabula.bold-themes.com/wavy/courses/schedule/">Schedule</a>
-											</li>
-											<li id="menu-item-2746" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2746">
-												<a href="https://tabula.bold-themes.com/wavy/courses/workshop/">Workshop</a>
-											</li>
-											<li id="menu-item-2745" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2745">
-												<a href="https://tabula.bold-themes.com/wavy/courses/workshops-and-events/">Workshops
-													&amp; Events</a>
-											</li>
-											<li id="menu-item-2605" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2605">
-												<a href="https://tabula.bold-themes.com/wavy/courses/get-quote/">Get
-													Quote</a>
-											</li>
-										</ul>
+										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/courses/">Aulas</a>
 									</li>
-									<li id="menu-item-40" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-40">
-										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/blog/">Blog</a>
-										<ul class="sub-menu">
-											<li id="menu-item-1197" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1197">
-												<a href="https://tabula.bold-themes.com/wavy/blog/">Standard view</a>
-											</li>
-											<li id="menu-item-1198" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1198">
-												<a href="https://tabula.bold-themes.com/wavy/blog/?blog_list_view=columns">Columns
-													view</a>
-											</li>
-											<li id="menu-item-1199" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1199">
-												<a href="https://tabula.bold-themes.com/wavy/blog/?blog_list_view=simple">Simple
-													view</a>
-											</li>
-											<li id="menu-item-1205" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1205">
-												<a href="https://tabula.bold-themes.com/wavy/blog/blog-grid/">Grid
-													view</a>
-											</li>
-											<li id="menu-item-2951" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-has-children menu-item-2951">
-												<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/2018/01/02/10-reasons-why-studying-in-london-is-the-best/">Single
-													Post</a>
-												<ul class="sub-menu">
-													<li id="menu-item-2952" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2952">
-														<a href="https://tabula.bold-themes.com/wavy/2018/01/02/10-reasons-why-studying-in-london-is-the-best/">Standard
-															Post</a>
-													</li>
-													<li id="menu-item-2953" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2953">
-														<a href="https://tabula.bold-themes.com/wavy/2017/04/01/language-mistakes-made-by-english-speakers/">Grid
-															Gallery Post</a>
-													</li>
-													<li id="menu-item-2954" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2954">
-														<a href="https://tabula.bold-themes.com/wavy/2018/01/09/some-of-the-strangest-words-in-the-english-language/">Carousel
-															&amp; Columns Post</a>
-													</li>
-													<li id="menu-item-2955" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2955">
-														<a href="https://tabula.bold-themes.com/wavy/2017/07/20/why-do-people-study-for-ielts-qualification/">Quote
-															Post</a>
-													</li>
-													<li id="menu-item-2956" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2956">
-														<a href="https://tabula.bold-themes.com/wavy/2017/05/08/business-english-why-its-must-to-learn-it/">Link
-															Post</a>
-													</li>
-													<li id="menu-item-2957" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2957">
-														<a href="https://tabula.bold-themes.com/wavy/2018/01/09/30-english-idioms-to-help-you-sound-like-native/">Audio
-															Post</a>
-													</li>
-													<li id="menu-item-2958" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-2958">
-														<a href="https://tabula.bold-themes.com/wavy/2018/01/09/top-tips-when-taking-your-fce-and-cae-exam/">Video
-															Post</a>
-													</li>
-												</ul>
-											</li>
-											<li id="menu-item-59" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-59">
-												<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/galleries/">Galleries</a>
-												<ul class="sub-menu">
-													<li id="menu-item-1219" class="menu-item menu-item-type-post_type menu-item-object-portfolio menu-item-1219">
-														<a href="https://tabula.bold-themes.com/wavy/galleries/future-of-language-teaching-2019/">Standard
-															Post</a>
-													</li>
-													<li id="menu-item-1215" class="menu-item menu-item-type-post_type menu-item-object-portfolio menu-item-1215">
-														<a href="https://tabula.bold-themes.com/wavy/galleries/summer-course-uk-london-2019/">Carousel
-															Post</a>
-													</li>
-													<li id="menu-item-1216" class="menu-item menu-item-type-post_type menu-item-object-portfolio menu-item-1216">
-														<a href="https://tabula.bold-themes.com/wavy/galleries/cambridge-language-certificate-in-2018/">Grid
-															Gallery</a>
-													</li>
-													<li id="menu-item-1217" class="menu-item menu-item-type-post_type menu-item-object-portfolio menu-item-1217">
-														<a href="https://tabula.bold-themes.com/wavy/galleries/i-to-i-tefl-teacher-training/">Video
-															Post</a>
-													</li>
-													<li id="menu-item-1218" class="menu-item menu-item-type-post_type menu-item-object-portfolio menu-item-1218">
-														<a href="https://tabula.bold-themes.com/wavy/galleries/ise-teacher-training-workshop/">Audio
-															Post</a>
-													</li>
-												</ul>
-											</li>
-										</ul>
+									<li id="menu-item-2168" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-2168">
+										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/courses/">Eventos e workshops</a>
 									</li>
-									<li id="menu-item-41" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-41">
-										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/shop/">Shop</a>
-										<ul class="sub-menu">
-											<li id="menu-item-152" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-152">
-												<a href="https://tabula.bold-themes.com/wavy/shop/">Products</a>
-											</li>
-											<li id="menu-item-47" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47">
-												<a href="https://tabula.bold-themes.com/wavy/shop/cart/">Cart</a>
-											</li>
-											<li id="menu-item-46" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-46">
-												<a href="https://tabula.bold-themes.com/wavy/shop/checkout/">Checkout</a>
-											</li>
-											<li id="menu-item-45" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-45">
-												<a href="https://tabula.bold-themes.com/wavy/shop/my-account/">My
-													Account</a>
-											</li>
-										</ul>
-									</li>
-									<li id="menu-item-2219" class="btMenuWideDropdown menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-2219 btMenuWideDropdownCols-3">
+									<!-- <li id="menu-item-2219" class="btMenuWideDropdown menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-2219 btMenuWideDropdownCols-3">
 										<div class="subToggler"></div><a href="https://tabula.bold-themes.com/wavy/home/home-music-courses/#">Elements</a>
 										<ul class="sub-menu">
 											<li id="menu-item-1296" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1296">
@@ -350,7 +153,7 @@
 												</ul>
 											</li>
 										</ul>
-									</li>
+									</li> -->
 								</ul>
 							</nav>
 						</div><!-- .menuPort -->
