1f3e3b02a0cf210f678610e7464cb18e88d03131
Elton Leão <elton.leao@peopleinteractive.com.br>
2024-05-02 21:43:16 -0300

    Add bootstrap styles and scripts to theme functions.php

--- wordpress/wp-content/themes/suzukipetropolis/functions.php
+++ wordpress/wp-content/themes/suzukipetropolis/functions.php
@@ -33,3 +33,11 @@ function my_custom_scripts() {
     wp_enqueue_script('my-custom-js', get_stylesheet_directory_uri() . '/main.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'my_custom_scripts');
+
+
+//add boostrap
+function add_bootstrap() {
+    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
+    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '4.5.2', true);
+}
+add_action('wp_enqueue_scripts', 'add_bootstrap');
--- wordpress/wp-content/themes/suzukipetropolis/home.php
+++ wordpress/wp-content/themes/suzukipetropolis/home.php
@@ -5,112 +5,11 @@ Template Name: Home
 get_header();
 ?>
 
-
-
-<div id="content" class="site-content">
-    <div class="ast-container">
-
-
-        <div id="primary" class="content-area primary">
-
-
-            <main id="main" class="site-main">
-                <article class="post-77 page type-page status-publish ast-article-single" id="post-77" itemtype="https://schema.org/CreativeWork" itemscope="itemscope">
-
-
-                    <header class="entry-header ast-no-thumbnail ast-no-title ast-header-without-markup">
-                    </header> <!-- .entry-header -->
-
-
-                    <div class="entry-content clear" itemprop="text">
-
-
-                        <div data-elementor-type="wp-page" data-elementor-id="77" class="elementor elementor-77">
-                            <div class="elementor-element elementor-element-d8eedbd e-flex e-con-boxed e-con e-parent e-lazyloaded" data-id="d8eedbd" data-element_type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
-                                <div class="e-con-inner">
-                                    <div class="elementor-element elementor-element-42003bc masthead_curve_bg elementor-widget elementor-widget-image" data-id="42003bc" data-element_type="widget" data-widget_type="image.default">
-                                        <div class="elementor-widget-container">
-                                            <style>
-                                                /*! elementor - v3.21.0 - 30-04-2024 */
-                                                .elementor-widget-image {
-                                                    text-align: center
-                                                }
-
-                                                .elementor-widget-image a {
-                                                    display: inline-block
-                                                }
-
-                                                .elementor-widget-image a img[src$=".svg"] {
-                                                    width: 48px
-                                                }
-
-                                                .elementor-widget-image img {
-                                                    vertical-align: middle;
-                                                    display: inline-block
-                                                }
-                                            </style> <img fetchpriority="high" decoding="async" width="2500" height="620" src="https://suzukipetropolis.com/wp-content/uploads/2024/05/Wave_White_bottom_right_shape_01.png" class="attachment-full size-full wp-image-109" alt="" srcset="https://suzukipetropolis.com/wp-content/uploads/2024/05/Wave_White_bottom_right_shape_01.png 2500w, https://suzukipetropolis.com/wp-content/uploads/2024/05/Wave_White_bottom_right_shape_01-300x74.png 300w, https://suzukipetropolis.com/wp-content/uploads/2024/05/Wave_White_bottom_right_shape_01-1024x254.png 1024w, https://suzukipetropolis.com/wp-content/uploads/2024/05/Wave_White_bottom_right_shape_01-768x190.png 768w, https://suzukipetropolis.com/wp-content/uploads/2024/05/Wave_White_bottom_right_shape_01-1536x381.png 1536w, https://suzukipetropolis.com/wp-content/uploads/2024/05/Wave_White_bottom_right_shape_01-2048x508.png 2048w" sizes="(max-width: 2500px) 100vw, 2500px">
-                                        </div>
-                                    </div>
-                                    <div class="elementor-element elementor-element-9552ff4 e-con-full e-flex e-con e-child" data-id="9552ff4" data-element_type="container">
-                                        <div class="elementor-element elementor-element-e02f891 elementor-widget elementor-widget-html" data-id="e02f891" data-element_type="widget" data-widget_type="html.default">
-                                            <div class="elementor-widget-container">
-                                                <div class="masthead_info_container">
-                                                    <h4>CENTRO DE ARTES</h4>
-                                                    <h1>SUZUKI DE PETRÓPOLIS</h1>
-                                                    <div>
-                                                        <a href="/#" target="_self">
-                                                            <span>
-                                                                QUERO SER SUZUKI!
-                                                            </span>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="elementor-element elementor-element-8444179 e-con-full e-flex e-con e-child" data-id="8444179" data-element_type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
-                                        <div class="elementor-element elementor-element-0112843 masthead_main_image elementor-widget elementor-widget-image" data-id="0112843" data-element_type="widget" data-widget_type="image.default" style="transform: translateY(-14.9356px);">
-                                            <div class="elementor-widget-container">
-                                                <img decoding="async" width="770" height="690" src="https://suzukipetropolis.com/wp-content/uploads/2024/05/home_hero_01.png" class="attachment-full size-full wp-image-79" alt="" srcset="https://suzukipetropolis.com/wp-content/uploads/2024/05/home_hero_01.png 770w, https://suzukipetropolis.com/wp-content/uploads/2024/05/home_hero_01-300x269.png 300w, https://suzukipetropolis.com/wp-content/uploads/2024/05/home_hero_01-768x688.png 768w" sizes="(max-width: 770px) 100vw, 770px">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="elementor-element elementor-element-82d4bb3 e-con-full masthead_floating_note1 e-flex e-con e-child" data-id="82d4bb3" data-element_type="container">
-                                        <div class="elementor-element elementor-element-db683f1 elementor-widget elementor-widget-image" data-id="db683f1" data-element_type="widget" data-widget_type="image.default">
-                                            <div class="elementor-widget-container">
-                                                <img loading="lazy" decoding="async" width="121" height="102" src="https://suzukipetropolis.com/wp-content/uploads/2024/05/floating_notes_01.png" class="attachment-full size-full wp-image-145" alt="" style="transform: translateY(-0.143705px);">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="elementor-element elementor-element-d80a5a4 e-con-full masthead_floating_note2 e-flex e-con e-child" data-id="d80a5a4" data-element_type="container">
-                                        <div class="elementor-element elementor-element-6b3e292 elementor-widget elementor-widget-image" data-id="6b3e292" data-element_type="widget" data-widget_type="image.default">
-                                            <div class="elementor-widget-container">
-                                                <img loading="lazy" decoding="async" width="157" height="186" src="https://suzukipetropolis.com/wp-content/uploads/2024/05/floating_notes_02.png" class="attachment-large size-large wp-image-155" alt="" style="transform: translateY(3.33945px);">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                    </div><!-- .entry-content .clear -->
-
-
-
-                </article><!-- #post-## -->
-
-            </main><!-- #main -->
-
-
-        </div><!-- #primary -->
-
-
-    </div> <!-- ast-container -->
+<div class="container">
+    <?php
+        include_once(__DIR__ . '/includes/masthead.php');
+    ?>
 </div>
-
-
 
 <?php
 get_footer();  // Chama o rodapé do tema
